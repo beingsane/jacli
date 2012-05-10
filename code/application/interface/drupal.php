db6faad16cfeeb7db4717fc20bb78d6dfff56952
Nikolai Plath <der.el.kuku@gmail.com>
2012-05-10 15:10:08 -0500

    Add a Drupal interface

---
+++ code/application/interface/drupal.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * User: elkuku
+ * Date: 10.05.12
+ * Time: 12:40
+ */
+class AcliApplicationInterfaceDrupal extends AcliApplicationInterface
+{
+	protected $name = 'joomla-cms';
+
+	public function createAdminUser(AcliModelDatabase $db)
+	{
+		// TODO: Implement createAdminUser() method.
+	}
+
+	public function createConfig()
+	{
+		// TODO: Implement createConfig() method.
+	}
+
+	public function cleanup()
+	{
+		// TODO: Implement cleanup() method.
+	}
+
+	public function setupDatabase()
+	{
+		// TODO: Implement setupDatabase() method.
+
+		$this->config->set('db_name', $this->config->get('target'));
+
+		$dbModel = new AcliModelDatabase($this->config);
+
+		$this->out(sprintf('Creating database %s ...', $this->config->get('db_name')), false);
+		$dbModel->createDB();
+		$this->out('ok');
+
+		return $this;
+	}
+
+	/**
+	 * Displays a result message.
+	 *
+	 * @return array
+	 */
+	public function getResultMessage()
+	{
+		// TODO: Implement getResultMessage() method.
+
+		$message = array();
+
+		$message[] = '';
+		$message[] = 'Drupal has been installed succesfully.';
+		$message[] = '';
+		$message[] = 'Credentials:';
+		$message[] = 'Database       : ' . $this->config->get('db_name');
+		$message[] = 'Admin user     : ' . $this->config->get('admin_user');
+		$message[] = 'Admin password : ' . $this->config->get('admin_password');
+		$message[] = '';
+
+		return $message;
+
+	}
+
+	public function getBrowserLinks()
+	{
+		// TODO: Implement getBrowserLinks() method.
+
+		return array('Drupal site' => '');
+	}
+}
---
+++ config/application/drupal.dist.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * User: elkuku
+ * Date: 05.05.12
+ * Time: 17:55
+ */
+
+class AcliConfigDrupal extends JConfig
+{
+	// Database credentials
+	public $db_type = 'mysqli';
+	public $db_host = 'localhost';
+	public $db_user = 'root';
+	public $db_pass = '';
+	public $db_prefix = 'drupal_';
+
+	// Admin user credentials
+	public $admin_user = 'admin';
+	public $admin_password = 'test';
+	public $admin_email = 'demo@example.com';
+
+	// http://localhost
+	public $httpBase = 'http://joomla.tests';
+
+	public $patchDir = '';
+
+	public $patches = array();
+}
