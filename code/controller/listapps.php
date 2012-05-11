717a035d697c87a23384389e129d8153e17c229d
Nikolai Plath <der.el.kuku@gmail.com>
2012-05-11 15:12:18 -0500

    Add controllers

---
+++ code/controller/listapps.php
@@ -0,0 +1,27 @@
+<?php
+	/**
+	 * User: elkuku
+	 * Date: 11.05.12
+	 * Time: 11:36
+	 */
+class AcliControllerListapps extends JControllerBase
+{
+
+	/**
+	 * Execute the controller.
+	 *
+	 * @return  boolean  True if controller finished execution, false if the controller did not
+	 *                   finish execution. A controller might return false if some precondition for
+	 *                   the controller to run has not been satisfied.
+	 *
+	 * @since            12.1
+	 * @throws  LogicException
+	 * @throws  RuntimeException
+	 */
+	public function execute()
+	{
+		$model = new AcliModelTargetapp;
+
+		$model->listApplications();
+	}
+}
---
+++ code/controller/listtargets.php
@@ -0,0 +1,29 @@
+<?php
+	/**
+	 * User: elkuku
+	 * Date: 11.05.12
+	 * Time: 11:36
+	 */
+class AcliControllerListtargets extends JControllerBase
+{
+
+	/**
+	 * Execute the controller.
+	 *
+	 * @return  boolean  True if controller finished execution, false if the controller did not
+	 *                   finish execution. A controller might return false if some precondition for
+	 *                   the controller to run has not been satisfied.
+	 *
+	 * @since            12.1
+	 * @throws  LogicException
+	 * @throws  RuntimeException
+	 */
+	public function execute()
+	{
+		$state = new JRegistry(JFactory::getApplication()->getConfig());
+
+		$model = new AcliModelDeploy($state);
+
+		$model->listTargets();
+	}
+}
