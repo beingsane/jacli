fe1f3569b2c492f2a19062c1a39a32b6a37dc4a0
Nikolai Plath <der.el.kuku@gmail.com>
2012-05-12 19:55:27 -0500

    Add web controllers

---
+++ code/controllerweb/default.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * User: elkuku
+ * Date: 12.05.12
+ * Time: 13:05
+ */
+
+class JacliControllerWebDefault extends JControllerBase
+{
+
+	/**
+	 * Execute the controller.
+	 *
+	 * @return  boolean  True if controller finished execution, false if the controller did not
+	 *                   finish execution. A controller might return false if some precondition for
+	 *                   the controller to run has not been satisfied.
+	 *
+	 * @since            12.1
+	 * @throws  LogicException
+	 * @throws  RuntimeException
+	 */
+	public function execute()
+	{
+		// TODO: Implement execute() method.
+	}
+}
---
+++ code/controllerweb/deploy.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User: elkuku
+ * Date: 12.05.12
+ * Time: 13:05
+ */
+
+class JacliControllerwebDeploy extends JControllerBase
+{
+
+	/**
+	 * Execute the controller.
+	 *
+	 * @return  boolean  True if controller finished execution, false if the controller did not
+	 *                   finish execution. A controller might return false if some precondition for
+	 *                   the controller to run has not been satisfied.
+	 *
+	 * @since            12.1
+	 * @throws  LogicException
+	 * @throws  RuntimeException
+	 */
+	public function execute()
+	{
+		// TODO: Implement execute() method.
+
+		if (JFactory::getApplication()->input->get('target')):
+			$model = new JacliModelDeploy(new JRegistry(JFactory::getApplication()->getConfig()));
+
+			$model->deploy();
+		endif;
+	}
+}
